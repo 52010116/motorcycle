26a89132ef7093e24668d937d594dcf48267c31d
TheDorili <dorian.pfeiler@me.com>
2023-01-25 10:02:18 +0100

    made doc partshop

--- src/app/components/parts/parts.service.ts
+++ src/app/components/parts/parts.service.ts
@@ -11,10 +11,11 @@ export class PartService  {
   constructor(private http: HttpClient) { }
     url = 'http://localhost:3000/parts';
   
+// Suche nach Ersatzteilen mit partName und fitsModel. 
 
   find(partName: string, fitsModel: string): Observable<SpareParts[]> {
 
-
+    // const url zu db.json
     const url = 'http://localhost:3000/parts';
 
 
@@ -27,7 +28,7 @@ export class PartService  {
         .set('partName', partName)
         .set('fitsModel', fitsModel);
 
-
+//Sendet get Anfrage an den Endpunkt und gibt ein observable zurück
     return this.http.get<SpareParts[]>(url, {headers, params});
 
 
@@ -35,15 +36,21 @@ export class PartService  {
     }
 
 
-
+// entfernt ersatzteile aus der datenbank anhand der ID, 
+// Sendet DELETE Anfrage an den angegebenen Endpunkt mit den Daten des Ersatzteiles und gibt ein observable zurück, das die Antwort ausgibt
     removePartEntry(SpareParts: any):Observable<SpareParts> {
       const headers = new HttpHeaders()
         .set('Accept', 'application/json');
       return this.http.delete<SpareParts>(this.url+"/"+SpareParts.id, {headers})
     }
+// CreatePost für neues ersatzteil, sendet post-anfrage an Endpunkt
     createPost(data: any): Observable<any> {
       return this.http.post('http://localhost:3000/parts', data);
     }
+// Die Methode updatePost wird verwendet, um ein ersatzteil anhand seiner ID zu aktualisieren. 
+//Sie sendet eine PUT-Anfrage an den angegebenen Endpunkt mit der ID und den Daten des Ersatzteils 
+//und gibt ein Observable zurück, das das aktualisierte Ersatzteil ausgibt
+
     updatePost(id: any, data: any): Observable<any> {
       return this.http.put(`${'http://localhost:3000/parts'}/${id}`, data);
   } 
